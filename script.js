f398ea4b777045e5d83b40c8cece290c1f488577
Shubham628169 <skvatas2684@gmail.com>
2024-09-14 19:03:08 +0530

    animations added

--- script.js
+++ script.js
@@ -5,26 +5,44 @@ backSpeed:150,
 looped :true
 })*/
 let p = document.querySelector(".text-small");
-const text = "We will serve you better services ever.";
+
+
+const text = [
+    "Welcome to repairHUB!",
+    "We will serve you better services ever.",
+    "Experience our professional repair services."
+    ];
+
+    let textIndx=0;
+
 
 function typewriterEffect(element, text, i = 0) {
-    
+     
+    const currentText = text[textIndx];
     if (i === 0) {
         element.textContent =''; 
     }
     
     
-    element.textContent += text[i];
+    element.textContent += currentText[i];
     
 
-    if (i === text.length - 1) {
+    if (i === currentText.length - 1) {
+       //Restart the animation..
+    
+        setTimeout(() => {
+            textIndx=(textIndx + 1) % text.length;
+            typewriterEffect(element, text, 0);
+        }, 1500)
+
         return;
     }
     
-
     setTimeout(() => {
         typewriterEffect(element, text, i + 1);
-    }, 50); 
+    }, 70)
+    
 }
 
 typewriterEffect(p, text);
+
